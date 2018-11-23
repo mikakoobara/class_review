cba8454f376e6a30f0b5bd23c81cef5d957c4d42
obaramikako <quartersilver@gmail.com>
2018-11-23 21:07:55 +0900

    B-1.終了

--- customer.py
+++ customer.py
@@ -1,9 +1,15 @@
-class Customer:
+class Customer(object):
+    def __init__(self, first_name, family_name):
+        self.first_name = first_name
+        self.family_name = family_name
+
+    def full_name(self):
+        return self.first_name + " " + self.family_name
 
 
+if __name__ == '__main__':
+    ken = Customer(first_name="Ken", family_name="Tanaka")
+    ken.full_name()  # "Ken Tanaka" という値を返す
 
-    # ken = Customer(first_name="Ken", family_name="Tanaka")
-    # ken.full_name()  # "Ken Tanaka" という値を返す
-    #
-    # tom = Customer(first_name="Tom", family_name="Ford")
-    # tom.full_name()  # "Tom Ford" という値を返す
+    tom = Customer(first_name="Tom", family_name="Ford")
+    tom.full_name()  # "Tom Ford" という値を返す

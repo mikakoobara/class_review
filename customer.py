30434060564865d56c48f9b9f0f57d7d4a7cf308
obaramikako <quartersilver@gmail.com>
2018-11-23 15:26:01 +0900

    First commit

---
+++ customer.py
@@ -0,0 +1,9 @@
+class Customer:
+
+
+
+    # ken = Customer(first_name="Ken", family_name="Tanaka")
+    # ken.full_name()  # "Ken Tanaka" という値を返す
+    #
+    # tom = Customer(first_name="Tom", family_name="Ford")
+    # tom.full_name()  # "Tom Ford" という値を返す

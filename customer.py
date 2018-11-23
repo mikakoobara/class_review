40e69bfe110592336d4ab11e410c0640c218910e
obaramikako <quartersilver@gmail.com>
2018-11-23 22:14:21 +0900

    B-4.終了

--- customer.py
+++ customer.py
@@ -15,13 +15,16 @@ class Customer(object):
         else:
             return 1200
 
+    def info_csv(self):
+        return self.full_name() + "," + str(self.age) + "," + str(self.entry_fee())
+
 
 if __name__ == '__main__':
     ken = Customer(first_name="Ken", family_name="Tanaka", age=15)
-    ken.entry_fee()  # 1000 という値を返す
+    ken.info_csv()  # "Ken Tanaka,15,1000" という値を返す
 
     tom = Customer(first_name="Tom", family_name="Ford", age=57)
-    tom.entry_fee()  # 1500 という値を返す
+    ken.info_csv()  # "Tom Ford,57,1500" という値を返す
 
     ieyasu = Customer(first_name="Ieyasu", family_name="Tokugawa", age=73)
-    ieyasu.entry_fee()  # 1200 という値を返す
+    ieyasu.info_csv()  # "Ieyasu Tokugawa,73,1200" という値を返す
